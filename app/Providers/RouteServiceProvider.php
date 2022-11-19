03e002d40ebbb96b5315cc4f5f574767a7ff2775
Hinduck <20520450@gm.uit.edu.vn>
2022-11-19 08:30:51 +0700

    Done 4 clips

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,8 +17,9 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/home';
+    public const HOME = '/';
 
+    protected $namespace = 'App\Http\Livewire';
     /**
      * Define your route model bindings, pattern filters, and other route configuration.
      *
@@ -30,10 +31,12 @@ class RouteServiceProvider extends ServiceProvider
 
         $this->routes(function () {
             Route::middleware('api')
+                ->namespace($this->namespace)
                 ->prefix('api')
                 ->group(base_path('routes/api.php'));
 
             Route::middleware('web')
+                ->namespace($this->namespace)
                 ->group(base_path('routes/web.php'));
         });
     }
